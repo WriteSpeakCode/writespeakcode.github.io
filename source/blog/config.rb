63f0f6bbd1c53e0e639af79962f6c19cdd0ec49b
echappen <elenichappen@gmail.com>
2015-01-27 16:19:39 -0600

    added initial blog functionality

--- source/blog/config.rb
+++ source/blog/config.rb
@@ -1,4 +1,37 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -14,10 +47,10 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -25,8 +58,8 @@
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -36,15 +69,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
- configure :development do
-   activate :livereload
- end
-
- # add blog functionality
- activate :blog do |blog|
-  blog.paginate = true
-  blog.prefix = "blog"
- end
+# activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -58,10 +83,6 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
-
-#Bower support
-sprockets.append_path File.join root, 'bower_components'
-sprockets.import_asset 'jquery'
 
 # Build-specific configuration
 configure :build do

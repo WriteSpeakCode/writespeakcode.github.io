d6b55393333fd2a608d291afeb0e7d435b2fd0c5
echappen <elenichappen@gmail.com>
2015-02-05 08:20:29 -0600

    fixed collapsing navbar by adding javascript from refills

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,16 @@
 //= require 'jquery'
 //= require_tree .
+
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
